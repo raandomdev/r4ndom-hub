6b145cf9e88bcc689fa6cba91be2ee6b4c2ec241
spacedev <ch2769267@gmail.com>
2025-08-30 23:47:59 -0400

    Update source.lua

--- source.lua
+++ source.lua
@@ -29,7 +29,7 @@ local OrionLib = {
 local Icons = {}
 
 local Success, Response = pcall(function()
-	Icons = HttpService:JSONDecode(game:HttpGetAsync("https://raw.githubusercontent.com/evoincorp/lucideblox/master/src/modules/util/icons.json")).icons
+	Icons = HttpService:JSONDecode(game:HttpGetAsync("https://raw.githubusercontent.com/raandomdev/r4ndom-hub/refs/heads/uis/icons.json")).icons
 end)
 
 if not Success then
